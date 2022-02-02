03db17fdb1c8422ae6d0fc57e0f56de451f7eada
Sam.sr <samuel.j.robbins@gmail.com>
2022-02-02 13:51:23 -0800

    js and css update
    add hover and opacity to nav and project divs

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
- $(document).ready(function(){
-    $("button").click(function(){
-      $("div").animate({left: '250px'});
-    });
+$(document).ready(function() {
+    //set opacity to 0.4for all the images
+    //opac
+$('.photos').css('opacity', 0.4);
+
+    $('.photos').click(
+        function(){
+            $(this).find('img').stop().fadeto('slow', 1);
+        },
+        function(){
+            $(this).find('img').stop().fadeto('slow', .04);
+            
+
+//     $(document).ready(function(){
+//     $("button").click(function(){
+//       $("div").animate({left: '250px'});
+//     });
   });
+});
